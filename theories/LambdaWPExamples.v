0073efff991603afac581ea8d174bb1a253e8ddb
charguer <arthur@chargueraud.org>
2019-01-29 16:29:49 +0100

    example sources

--- theories/LambdaWPExamples.v
+++ theories/LambdaWPExamples.v
@@ -263,16 +263,151 @@ Proof using. introv E N. applys Local_erase. case_if*. Qed.
 
 
 
+
+(* ********************************************************************** *)
+(* * Point *)
+
+Module Factorial.
+
+Parameter facto : int -> int.
+Parameter facto_zero : facto 0 = 1.
+Parameter facto_one : facto 1 = 1.
+Parameter facto_succ : forall n, n >= 1 -> facto n = n * facto(n-1).
+
+(*
+
+  let rec facto_rec n =
+    if n <= 1 then 1 else n * facto_rec (n-1)
+
+  let facto_ref_rec_up n =
+    let r = ref 1 in
+    let rec f x =
+      if x <= n
+        then r := !r * x; f (x+1) in
+    f 1;
+    !r
+
+  let facto_ref_rec_down n =
+    let r = ref 1 in
+    let rec f n =
+      if n > 1
+        then r := !r * n; f (n-1) in
+    f n; 
+    !r
+
+  let facto_for n =
+    let r = ref 1 in
+    for x = 1 to n do
+      r := !r * x;
+    done;
+    !r
+
+  let facto_for_down n =
+    let r = ref 1 in
+    for x = 0 to n-1 do 
+      r := !r * (n-x);
+    done;
+    !r
+
+  let facto_for_downto n =
+    let r = ref 1 in
+    for x = n downto 1 do 
+      r := !r * x;
+    done;
+    !r
+
+  let facto_for_downto2 n =
+    let r = ref 1 in
+    for x = n downto 2 do 
+      r := !r * x;
+    done;
+    !r
+
+  let facto_while_up n =
+    let r = ref 1 in
+    let x = ref 1 in
+    while get x <= n do
+      r := !r * !x;
+      incr x;
+    done;
+    !r
+
+  let facto_while_down n =
+    let r = ref 1 in
+    let x = ref n in
+    while get x > 1 do
+      r := !r * !x;
+      decr x;
+    done;
+    !r
+*)
+
+End Factorial.
+
+
+
+(* ********************************************************************** *)
+(* * Point *)
+
+Module Point.
+
+Definition X : field := 0%nat.
+Definition Y : field := 1%nat.
+Definition S : field := 2%nat.
+
+(*
+Definition Point (p:loc) : hprop :=
+  \exists x y s, p ~> Record`{ X := x; Y := y; S := s } \* [ s = x + y ].
+*)
+
+
+End Point.
+
+
+(* ********************************************************************** *)
+(* * Mutable lists *)
+
+
+Module MList.
+
+(*
+  - For recursive predicate: would be useful to recall the duality between
+   `Fixpoint` and `Inductive` for defining predicates, taking the example of `In` and `Forall` on lists.
+*)
+(*
+    ```
+       Fixpoint MList A `{EA:Enc A} (L:list A) (p:loc) : hprop :=
+         Hexists v, p ~> v  \*
+         match L with
+         | nil => \[v = Nil]
+         | x::L => Hexists p', \[v = Cons(x,p')] \* (p' ~> MList L')
+         end.
+    ```
+
+
+   length : using recursion + using loop
+   copy : using recursion + using loop
+   append (destructive, or non-destructive)
+   mem
+   count
+   in-place reversal
+   cps-append (bonus example)
+   split 
+   combine  
+   basic sorting on list of integers, e.g. merge sort, insertion sort
+
+*)
+
+End MList.
+
+
+(* ********************************************************************** *)
+(* * Basic *)
+
+Module Basic.
+
 (* ---------------------------------------------------------------------- *)
-(* * Example proofs *)
-
-Module Test.
-
-
-
-
-(* ---------------------------------------------------------------------- *)
-(* ** Increment *)
+(** Incr *)
 
 Definition val_incr : val :=
   ValFun 'p :=
@@ -312,6 +447,13 @@ Proof using.
   auto.
 Qed.
 
+Lemma Triple_incr_frame : forall (p1 p2:loc) (n1 n2:int),
+  TRIPLE (val_incr p1)
+    PRE (p1 ~~> n1 \* p2 ~~> n2)
+    POST (fun (r:unit) => (p1 ~~> (n1+1) \* p2 ~~> n2)).
+Proof using.
+  skip.
+Qed.
 
 (* TODO SHOULD BE:
 
@@ -331,11 +473,151 @@ then just:
 
 *)
 
+(*
+
+(* ---------------------------------------------------------------------- *)
+(** Swap *)
+
+Definition val_swap :=
+  ValFun 'p 'q :=
+    Let 'x := val_get 'p in
+    Let 'y := val_get 'q in
+    val_set 'p 'y ;;;
+    val_set 'q 'x.
+
+Lemma Triple_swap_neq : forall A1 A2 `{EA1:Enc A1} `{EA2:Enc A2} (v:A1) (w:A2) p q,
+  Triple (val_swap ``p ``q)
+    PRE (p ~~> v \* q ~~> w)
+    POST (fun (r:unit) => p ~~> w \* q ~~> v).
+Proof using.
+  xcf. xapps. xapps. xapps. xapps. hsimpl~.
+Qed.
+
+Lemma Triple_swap_eq : forall A1 `{EA1:Enc A1} (v:A1) p,
+  Triple (val_swap ``p ``p)
+    PRE (p ~~> v)
+    POST (fun (r:unit) => p ~~> v).
+Proof using.
+  xcf. xapps. xapps. xapps. xapps. hsimpl~.
+Qed.
 
 
 (* ---------------------------------------------------------------------- *)
-(* ** Case without variables *)
+(** Succ using incr *)
 
+Definition val_succ_using_incr :=
+  ValFun 'n :=
+    Let 'p := val_ref 'n in
+    val_incr 'p ;;;
+    Let 'x := val_get 'p in
+    'x.
+
+Lemma triple_succ_using_incr : forall n,
+  triple (val_succ_using_incr n)
+    \[]
+    (fun r => \[r = n+1]).
+Proof using.
+  xcf. xapp as p. intros; subst. xapp~. intros _. xapps~.
+  (* not possible: applys local_erase. unfold cf_val. hsimpl. *)
+  xvals~.
+Qed.
+
+
+
+(* ---------------------------------------------------------------------- *)
+(** Basic let-binding example *)
+
+Definition val_example_let :=
+  ValFun 'n :=
+    Let 'a := 'n '+ 1 in
+    Let 'b := 'n '- 1 in
+    'a '+ 'b.
+
+Lemma Triple_val_example_let : forall n,
+  Triple (val_example_let n)
+    PRE \[]
+    POST (fun r => \[r = 2*n]).
+Proof using.
+  xcf. xapps. xapps. xapp. hsimpl. math.
+Qed.
+
+
+(* ---------------------------------------------------------------------- *)
+(** Basic let-binding example *)
+
+(*
+  let val_example_one_ref n =
+    let i = ref 0 in
+    incr i;
+    !i
+*)
+
+Definition val_example_one_ref :=
+  ValFun 'n :=
+    Let 'k := 'n '+ 1 in
+    Let 'i := 'ref 'k in
+    val_incr 'i ;;;
+    '!'i.
+
+Lemma Triple_val_example_one_ref : forall n,
+  Triple (val_example_one_ref n)
+    PRE \[]
+    POST (fun r => \[r = n+2]).
+Proof using.
+  xcf. xapps. xapps ;=> r. xapp~. xapp~. hsimpl. math.
+Qed.
+
+
+(* ---------------------------------------------------------------------- *)
+(** Basic let-binding two ref *)
+
+(*
+  let val_example_two_ref n =
+    let i = ref 0 in
+    let r = ref n in
+    decr r;
+    incr i;
+    r := !i + !r;
+    !i + !r
+*)
+
+Definition val_example_two_ref :=
+  ValFun 'n :=
+    Let 'i := 'ref 0 in
+    Let 'r := 'ref 'n in
+    val_decr 'r ;;;
+    val_incr 'i ;;;
+    Let 'i1 := '!'i in
+    Let 'r1 := '!'r in
+    Let 's := 'i1 '+ 'r1 in
+    'r ':= 's ;;;
+    Let 'i2 := '!'i in
+    Let 'r2 := '!'r in
+    'i2 '+ 'r2.
+
+Lemma Triple_val_example_two_ref : forall n,
+  Triple (val_example_two_ref n)
+    PRE \[]
+    POST (fun r => \[r = n+1]).
+Proof using.
+  xcf. xapp ;=> i. xapp ;=> r.
+  xapp~. xapp~. xapps. xapps. xapps. xapps~.
+  xapps. xapps. xapps.
+  hsimpl. math.
+Qed.
+
+*)
+
+End Basic.
+
+
+(* ********************************************************************** *)
+(* * Test *)
+
+Module Test.
+
+(* ---------------------------------------------------------------------- *)
+(* ** Case without variables *)
 
 Definition val_test1 : val :=
   ValFun 'p :=
@@ -391,9 +673,12 @@ Proof using.
 Admitted.
 
 
-(* ---------------------------------------------------------------------- *)
-(* ** Stack *)
+End Test.
 
+(* ********************************************************************** *)
+(* * Stack *)
+
+Module Stack.
 
 Definition val_is_empty : val :=
   ValFun 'p :=
@@ -573,7 +858,7 @@ Proof using.
 Qed.
 
 
-End Test.
+End Stack.
 
 
 
