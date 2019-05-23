e668701fd1b0623d2c3251c0d678e400afc495ff
charguer <arthur@chargueraud.org>
2019-05-23 17:26:36 +0200

    missing_files

---
+++ theories/SLFExtra.v
@@ -0,0 +1,39 @@
+(**
+
+Separation Logic Foundations
+
+Chapter: "Extra".
+
+Author: Arthur Charguéraud.
+License: MIT.
+
+*)
+
+Set Implicit Arguments.
+From Sep Require Export SLFHprop.
+From Sep Require SLFDirect.
+
+(** Implicit Types *)
+
+Implicit Types H : hprop.
+Implicit Types Q : val->hprop.
+
+
+(* ####################################################### *)
+(** * Separation Logic Triples *)
+
+(* ******************************************************* *)
+(** ** Structural rules *)
+
+(* ******************************************************* *)
+(** ** Rules for terms *)
+
+(* ******************************************************* *)
+(** ** Rules for primitives *)
+
+
+(* ####################################################### *)
+(** * Others *)
+
+(* ******************************************************* *)
+(** ** Conjunction and disjunction operators *)
