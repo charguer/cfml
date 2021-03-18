6c69f2f4d0135802ffa889a22b23f5f95b0ae868
François Pottier <francois.pottier@inria.fr>
2021-03-18 14:44:19 +0100

    Remove one go.sh.

--- examples/Sek/go.sh
+++
@@ -1,9 +0,0 @@
-rm *.cmj *_ml.v *.vo
-make -C ../.. install \
-&& make -f ../Makefile.dev _CoqProject \
-&& make -f ../Makefile.dev
-FILE=`basename \`pwd\``
-coqide -async-proofs off -async-proofs-command-error-resilience off ${FILE}_ml.v ${FILE}_proof.v &
-
-
-# make -f ../Makefile.dev `pwd`/UnitTests_ml.v
