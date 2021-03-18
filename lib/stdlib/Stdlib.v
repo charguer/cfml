d25fe490b1d1b027be0328e314e5d472bcd87384
François Pottier <francois.pottier@inria.fr>
2021-03-18 17:43:52 +0100

    Update Stdlib.v.

--- lib/stdlib/Stdlib.v
+++ lib/stdlib/Stdlib.v
@@ -1,11 +1,6 @@
 From CFML Require Import WPLib.
 Require Export Pervasives_ml Pervasives_proof.
 (* TODO: error without the require wplib, custom wp is undeclared *)
-
-(** Specifications for, e.g., the Array and List modules are to be imported
-    explicitly, via:
-[[
-   Require Array_ml Array_proof.
-   Require List_ml List_proof.
-]]
-*)
+Require Array_ml Array_proof.
+Require List_ml List_proof.
+Require Sys_ml Sys_proof.
