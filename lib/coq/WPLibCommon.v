cbe82cea948aa988820c7485f479eb595282f550
Alexandre Moine <alexandre@moine.me>
2021-07-09 22:34:49 +0200

    backport new xopen and xclose

--- lib/coq/WPLibCommon.v
+++ lib/coq/WPLibCommon.v
@@ -93,24 +93,55 @@ Notation "'TRIPLE' t 'PRE' H 'POST' Q" :=
 (* ********************************************************************** *)
 (** * Tactics for unfolding and folding representation predicates *)
 
-(* TODO *)
+(* ********************************************************************** *)
+(** ** Hints for xclose *)
+
+Inductive Xclose_hint {A:Type} (p:A) : forall (P:Prop), Type :=
+  | xclose_hint : forall (P:Prop) (R_eq:P), Xclose_hint p P.
+
+Lemma Xclose_hint_intro : forall (A:Type) (p:A) (P:Prop),
+  P ->
+  Xclose_hint p P.
+Proof using. intros. constructor*. Qed.
+
+Lemma Xclose_hint_elim : forall (A:Type) (p:A) (P:Prop),
+  Xclose_hint p P ->
+  P.
+Proof using. introv []. auto. Qed.
+
+Ltac xclose_hint_put p R_eq :=
+  let H := fresh "Hint_xclose" in
+  generalize (@Xclose_hint_intro _ p _ R_eq); intros H.
+
+Ltac xclose_hint_get p cont :=
+  match goal with H: Xclose_hint p ?P |- _ =>
+    let R_eq := constr:(@Xclose_hint_elim _ _ _ H) in
+    cont R_eq; clear H end.
+
+Declare Scope xtactics_scope.
+
+Notation "'Xclose_Hint' p" :=
+  (@Xclose_hint _ p _) (at level 0) : xtactics_scope.
+
+Open Scope xtactics_scope.
+
+(*
+Ltac xclose_hint_remove tt :=
+  match goal with H: @Xclose_hint _ _ _ _ |- _ => clear H end.
+*)
+
 
 (* ********************************************************************** *)
 (** ** Database *)
 
-(** The focus and unfocus databases are used to register MYSPECifications
+(** The focus database are used to register MYSPECifications
     for accessors to record fields, combined with focus/unfocus operations.
     See example/Stack/StackSized_proof.v for a demo of this feature. *)
 
 Definition database_xopen := True.
-Definition database_xclose := True.
 
 Notation "'RegisterOpen' T" := (Register database_xopen T)
   (at level 69) : wptactics_scope.
-
-Notation "'RegisterClose' T" := (Register database_xclose T)
-  (at level 69) : wptactics_scope.
-
 
 (* ********************************************************************** *)
 (** ** [xopen] *)
@@ -124,8 +155,6 @@ Notation "'RegisterClose' T" := (Register database_xclose T)
     then looks up in a database for the focus lemma [E] associated with
     the form [T], and then calls [xchange E].
 
-    [xopen_show p] shows the lemma found, it is useful for debugging.
-
     Example for registering a focusing lemma:
       Hint Extern 1 (RegisterOpen (Tree _)) => Provide tree_open.
     See [Demo_proof.v] for examples.
@@ -133,44 +162,49 @@ Notation "'RegisterClose' T" := (Register database_xclose T)
     Then, use: [xopen p].
 
     Variants:
-    - [xopenx t]  is short for [xopen t; xpull]
-
-    - [xopenxs t]  is short for [xopen t; xpulls]  // EXPERIMENTAL
+    - [xopen_repr R_open p] will use the given opening lemma.
 
     - [xopen2 p] to perform [xopen p] twice. Only applies when there
       is no existentials to be extracted after the first [xopen].
+ *)
 
-*)
+(** [xopen_get_lemma R cont] involves [cont R_eq], where
+    [R_eq] is the lemma bound to [R] using a command of the form
+    [Hint Extern 1 (RegisterOpen R) => Provide R_eq] . *)
+Ltac xopen_get_lemma R cont :=
+  ltac_database_get database_xopen R;
+  let R_eq := fresh "TEMP" in
+  intros R_eq; cont R_eq; clear R_eq.
 
-Ltac get_refocus_args tt :=
+(** [xgoal_state tt] returns the current state.
+    It extends [xgoal_pre] by also handling entailments. *)
+Ltac xgoal_state tt :=
   match goal with
-  | |- ?Q1 ===> ?Q2 => constr:((Q1,Q2))
-  | |- ?H1 ==> ?H2 => constr:((H1,H2))
-  | |- ?R ?H1 ?Q2 => constr:((H1,Q2))
-  (* TODO: gérer le cas de fonctions appliquées à R *)
+  | |- ?H1 ==> ?H2 => constr:(H1)
+  | |- ?Q1 ===> ?Q2 => constr:(Q1)
+  | _ => xgoal_pre tt
   end.
 
-Ltac get_refocus_constr_in H t :=
-  match H with context [ t ~> ?T ] => constr:(T) end.
+(* [xrepr_for_in p H] searches in heap predicate [H] for the
+   representation predicate associated with location [p] *)
+Ltac xrepr_for_in p H :=
+  match H with context [ p ~> ?T ] => get_head T end.
 
-Ltac xopen_constr t :=
-  match get_refocus_args tt with (?H1,?H2) =>
-  get_refocus_constr_in H1 t end.
+Ltac xopen_repr_core R_eq p :=
+  xclose_hint_put p R_eq;
+  xchange (>> R_eq p); xpull.
 
-Ltac xopen_core t :=
-  let C1 := xopen_constr t in
-  ltac_database_get database_xopen C1;
-  let K := fresh "TEMP" in
-  intros K; xchange (K t); clear K.
+Tactic Notation "xopen_repr" constr(R_eq) constr(p) :=
+  xopen_repr_core R_eq p.
 
-Ltac xopen_show_core t :=
-  let C1 := xopen_constr t in
-  pose C1;
-  try ltac_database_get database_xopen C1;
-  intros.
+Ltac xopen_internal_core p cont :=
+  let H := xgoal_state tt in
+  let R := xrepr_for_in p H in
+  xopen_get_lemma R cont.
 
-Tactic Notation "xopen_show" constr(t) :=
-  xopen_show_core t.
+Ltac xopen_core p :=
+  xopen_internal_core p ltac:(fun R_eq =>
+    xopen_repr_core R_eq p).
 
 Tactic Notation "xopen" constr(t) :=
   xopen_core t.
@@ -186,14 +220,6 @@ Tactic Notation "xopen2" "~" constr(x) :=
 Tactic Notation "xopen2" "*" constr(x) :=
   xopen2 x; auto_star.
 
-(* DEPRECATED
-Tactic Notation "xopenx" constr(t) :=
-  xopen t; xpull.
-Tactic Notation "xopenxs" constr(t) :=
-  xopen t; xpulls.
-*)
-
-
 (* ********************************************************************** *)
 (** ** [xclose] *)
 
@@ -203,64 +229,69 @@ Tactic Notation "xopenxs" constr(t) :=
     [xclose p] applies to a goal of the form
     [F H Q] or [H ==> H'] or [Q ===> Q'].
     It first searches for the pattern [p ~> T] in the pre-condition,
-    then looks up in a database for the unfocus lemma [E] associated with
-    the form [T], and then calls [xchange E].
-
-    [xclose_show p] shows the lemma found, it is useful for debugging.
-
-    Example for registering a focusing lemma:
-
-     Hint Extern 1 (RegisterClose (Ref Id (MNode _ _ _))) =>
-        Provide tree_node_close.
-
-    Then, use: [xclose p].
+    then looks up a Xclose_hint to get an unfocusing lemma.
 
     Variants:
+
+    - [xclose p L1 .. Ln] allows to pass arguments to the unfocusing
+      lemma.
+
+    - [xclose_repr R_close p] will use the given unfocusing lemma.
+
+    - [xcloses p] is a shortcut for [xclose p; xsimpl].
 
     - [xclose p1 .. pn] is short for [xclose p1; ..; xclose pn]
 
     - [xclose2 p] to perform [xclose p] twice.
+ *)
 
-    - [xclose (>> p X Y)] where the extra arguments are used to
-      provide explicit arguments to instantiate the "closing" lemma.
+(* [boxer_combine p args] builds a list of hint for instantiating
+   a lemma. [p] denotes one hint, and [args] denotes either one
+   hint or a list of hints of the form [>> arg1 .. argN]. *)
+Ltac boxer_combine p args := (* LATER: rename and move *)
+  match type of args with
+  | list Boxer => constr:(boxer p :: args)
+  | _ => constr:(boxer p :: boxer args :: nil)
+  end.
 
-*)
+Ltac xclose_repr_core R_eq p args :=
+  let all_args := boxer_combine p args in
+  let lemma := boxer_combine R_eq all_args in
+  xchange <- lemma.
 
+Tactic Notation "xclose_repr" constr(R_eq) constr(p) constr(args) :=
+  xclose_repr_core R_eq p args.
+Tactic Notation "xclose_repr" constr(R_eq) constr(p) :=
+  xclose_repr R_eq p (>>).
+(* LATER
+Tactic Notation "xclose_repr" constr(R_eq) constr(p) constr(arg1) constr(2) :=
+  xclose_repr R_eq p (>> arg1 arg2). *)
+Tactic Notation "xclose_repr" "*" constr(R_eq) constr(p) :=
+  xclose_repr R_eq p; auto_star.
+Tactic Notation "xclose_repr" "*" constr(R_eq) constr(p) constr(args) :=
+  xclose_repr R_eq p args; auto_star.
 
-Ltac xclose_get_ptr args :=
-  match args with (boxer ?t)::_ => t end.
+Tactic Notation "xcloses_repr" constr(R_eq) constr(p) constr(args) :=
+  xclose_repr R_eq p args; xsimpl.
+Tactic Notation "xcloses_repr" constr(R_eq) constr(p) :=
+  xcloses_repr R_eq p (>>).
+Tactic Notation "xcloses_repr" "*" constr(R_eq) constr(p) :=
+  xcloses_repr R_eq p; auto_star.
+Tactic Notation "xcloses_repr" "*" constr(R_eq) constr(p) constr(args) :=
+  xcloses_repr R_eq p args; auto_star.
 
-Ltac xclose_constr args :=
-  let t := xclose_get_ptr args in
-  match get_refocus_args tt with (?H1,?H2) =>
-  get_refocus_constr_in H1 t end.
+Ltac xclose_core p args :=
+  xclose_hint_get p ltac:(fun R_eq =>
+    xclose_repr_core R_eq p args).
 
-Ltac xclose_core args :=
-  let args := list_boxer_of args in
-  let C1 := xclose_constr args in
-  ltac_database_get database_xclose C1;
-  let K := fresh "TEMP" in
-  intros K;
-  let E := constr:( (boxer K)::args ) in
-  xchange E;
-  clear K.
-
-Ltac xclose_show_core args :=
-  let args := list_boxer_of args in
-  let C1 := xclose_constr args in
-  pose C1;
-  try ltac_database_get database_xclose C1;
-  intros.
-
-Tactic Notation "xclose_show" constr(t) :=
-  xclose_show_core t.
-
-Tactic Notation "xclose" constr(t) :=
-  xclose_core t.
-Tactic Notation "xclose" "~" constr(t) :=
-  xclose t; auto_tilde.
-Tactic Notation "xclose" "*" constr(t) :=
-  xclose t; auto_star.
+Tactic Notation "xclose" constr(p) constr(args) :=
+  xclose_core p args.
+Tactic Notation "xclose" constr(p) :=
+  xclose p (>>).
+Tactic Notation "xclose" "*" constr(p) :=
+  xclose p; auto_star.
+Tactic Notation "xclose" "*" constr(p) constr(args) :=
+  xclose p args; auto_star.
 
 Tactic Notation "xclose" constr(t1) constr(t2) :=
   xclose t1; xclose t2.
@@ -276,11 +307,14 @@ Tactic Notation "xclose2" "~" constr(x) :=
 Tactic Notation "xclose2" "*" constr(x) :=
   xclose2 x; auto_star.
 
-
-
-
-
-
+Tactic Notation "xcloses" constr(p) constr(args) :=
+  xclose p args; xsimpl.
+Tactic Notation "xcloses" constr(p) :=
+  xcloses p (>>).
+Tactic Notation "xcloses" "*" constr(p) :=
+  xcloses p; auto_star.
+Tactic Notation "xcloses" "*" constr(p) constr(args) :=
+  xcloses p args; auto_star.
 
 
 (************************************************************)
@@ -352,5 +386,3 @@ Notation "'MYSPEC'' t 'PRE' H1 'INV' H2 'RET' v 'POST' H3" :=
    format "'[v' 'MYSPEC''  t '/' '[' 'PRE'  H1  ']'  '/' '[' 'INV'  H2  ']'  '/' '[' 'RET'  v  'POST'  H3  ']'  ']'")
    : triple_scope.
 *)
-
-
