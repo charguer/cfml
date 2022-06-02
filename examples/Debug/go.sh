d1edc9361143c894b5f6257fe53f3625fff47304
charguer <arthur@chargueraud.org>
2022-06-02 10:39:52 +0200

    fix compilation

--- examples/Debug/go.sh
+++ examples/Debug/go.sh
@@ -1,7 +1,15 @@
+# usage: ./go.sh
+
 # For development purposes
+# make sure to first recompile your local TLC
 
 make -C ../../generator
 make -f Makefile.dev _CoqProject
-make -j4 -f Makefile.dev $1
+make -C ../../lib/coq -f Makefile.dev depend
+make -C ../../lib/coq -f Makefile.dev
+make -C ../../lib/stdlib -f Makefile.dev depend
+make -C ../../lib/stdlib -f Makefile.dev
+make -f Makefile.dev depend
+make -f Makefile.dev
 
 #coqide Debug_ml.v Debug_proof.v
