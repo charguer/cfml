1daf3ec4a6c643dc52062e3b4e82d0deaf2f8076
charguer <arthur@chargueraud.org>
2023-06-07 23:16:57 +0200

    compiles make

--- lib/stdlib/makedev.sh
+++ lib/stdlib/makedev.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
+export VERBOSE=1
+
 export ARG_EXAMPLEINCLUDE=EMPTY
 export DONT_INCLUDE_ALL_STDLIB_V=true
-
+cd ../..
 ARGS="$@"
 if [ -z ${ARGS} ]; then
   ARGS=libcoq
 fi
 
-cd ../..
 make -f Makefile.dev ${ARGS}
 
 # -d
