ec1aa206fda6129189952a99b1f5bd9b8a4f6c48
faremy <faremy03@gmail.com>
2023-07-04 11:11:43 +0200

    complétion de echunk

--- examples/Sek/EChunkImpl.ml
+++ examples/Sek/EChunkImpl.ml
@@ -1,96 +1,120 @@
 open Capacity
 
-(** Type pour des buffers circulaires de capacité fixe K   TODO TRANSLATE ALL*)
-
-type 'a echunk = { (* TODO: remove prefixes *)
+(** Fixed capacity circular buffers. *)
+type 'a echunk = {
   data : 'a array;
   mutable front : int;
   mutable size : int;
   default : 'a }
 
+(** [echunk_fields c]
+  returns the 4-tuple of the fields of [c].*)
 let echunk_fields c =
   c.data, c.front, c.size, c.default
 
+(** [echunk_default c]
+  returns the default value of c. *)
 let echunk_default c =
   c.default
-
+  
 let echunk_dummy d = {
   data = [||];
   front = 0;
   size = 0;
-  default = d
-}
+  default = d }
 
+(** [echunk_create d]
+  returns a fresh [echunk] with default value [d]. *)
 let echunk_create d = {
   data = Array.make capacity d;
   front = 0;
   size = 0;
-  default = d
-}
+  default = d }
 
+(** [echunk_is_empty c]
+  checks for emptiness of [c]. *)
 let echunk_is_empty c =
   c.size = 0
 
+(** [echunk_is_full c]
+  checks for fullness of [c]. *)
 let echunk_is_full c =
   c.size = capacity
 
+(** [echunk_size c]
+  returns the size of [c]. *)
 let echunk_size c =
   c.size
 
+(** [echunk_peek_back c]
+  returns the value at the back of [c]. *)
 let echunk_peek_back c =
   let back = wrap_up (c.front + c.size - 1) in
   c.data.(back)
 
+(** [echunk_pop_back c]
+  removes and returns the value at the back of [c]. *)
 let echunk_pop_back c =
-  let x = echunk_peek_back c in
   let new_size = c.size - 1 in
   c.size <- new_size;
   let i = wrap_up (c.front + new_size) in
+  let x = c.data.(i) in
   c.data.(i) <- c.default;
   x
 
+(** [echunk_push_back c x]
+  appends the value [x] to the back of [c]. *)
 let echunk_push_back c x =
   let old_size = c.size in
   c.size <- old_size + 1;
   let i = wrap_up (c.front + old_size) in
   c.data.(i) <- x
 
+(** [echunk_peek_front c]
+  returns the value at the front of [c]. *)
 let echunk_peek_front c =
   c.data.(c.front)
 
+(** [echunk_pop_front c]
+  removes and returns the value at the front of [c]. *)
 let echunk_pop_front c =
-  let x = echunk_peek_front c in
   let old_front = c.front in
   c.size <- c.size - 1;
   c.front <- wrap_up (old_front + 1);
+  let x = c.data.(old_front) in
   c.data.(old_front) <- c.default;
   x
 
+(** [echunk_push_front c x]
+  appends the value [x] to the front of [c]. *)
 let echunk_push_front c x =
   let new_front = wrap_down (c.front - 1) in
   c.size <- c.size + 1;
   c.front <- new_front;
   c.data.(new_front) <- x
 
+(** [echunk_get c i]
+  returns the value stored at the [i]-th position in [c]. *)
+let echunk_get c i =
+  let j = wrap_up (c.front + i) in
+  c.data.(j)
+
+(** [echunk_set c i x]
+  sets the value stored at the [i]-th position in [c] to [x]. *)
+let echunk_set c i x =
+  let j = wrap_up (c.front + i) in
+  c.data.(j) <- x
+
+(** [echunk_copy c]
+  returns a fresh copy of [c]. *)
 let echunk_copy c =
-  let data = Array.copy c.data in
-  { data = data;
+  let a = Array.copy c.data in
+  { data = a;
     front = c.front;
     size = c.size;
     default = c.default }
 
-(* let echunk_get c i =
-  c.data.(i)
-
-let echunk_set c i x =
-  c.data.(i) <- x *)
-
-(* let echunk_copy c =
-  let data = Array.copy c.data in
-  { data = data;
-    top = c.top;
-    default = c.default }
-
+(*
 let echunk_sub c size =
   let d = c.default in
   let t = c.data in
