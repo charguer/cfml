627672323706aed61c08940b32989668365d62bd
charguer <arthur@chargueraud.org>
2023-12-21 17:33:43 +0100

    proofs from basic chapter of SLF course

---
+++ examples/Tutorial/Basic.ml
@@ -0,0 +1,71 @@
+
+(** Same examples as in the first chapter of the course
+    Separation Logic Foundations
+    https://softwarefoundations.cis.upenn.edu/slf-current/Basic.html *)
+
+let incr r =
+  r := !r + 1
+
+let example_let n =
+  let a = n+1 in
+  let b = n-1 in
+  a + b
+
+let quadruple n =
+  let m = n + n in
+  m + m
+
+let inplace_double p =
+  let n = !p in
+  let m = n + n in
+  p := m
+
+let incr_two p q =
+  incr p;
+  incr q
+
+let aliased_call p =
+  incr_two p p
+
+let incr_first p q =
+  incr p
+
+let transfer p q =
+  let n = !p in
+  let m = !q in
+  let s = n + m in
+  p := s;
+  q := 0
+
+let ref_greater p =
+  let n = !p in
+  let m = n + 1 in
+  ref m
+
+let succ_using_incr n =
+  let p = ref n in
+  incr p;
+  ! p
+
+let get_and_free p =
+  let v = !p in
+  v
+
+let rec factorec n =
+  if n <= 1
+    then 1
+    else n * factorec (n-1)
+
+let rec repeat_incr p m =
+  if m > 0 then begin
+    incr p;
+    repeat_incr p (m - 1)
+  end
+
+let rec step_transfer p q =
+  if !q > 0 then begin
+    incr p;
+    decr q;
+    step_transfer p q
+  end
+
---
+++ examples/Tutorial/Basic_proof.v
@@ -0,0 +1,172 @@
+(** Same examples as in the first chapter of the course
+    Separation Logic Foundations
+    https://softwarefoundations.cis.upenn.edu/slf-current/Basic.html *)
+
+
+Set Implicit Arguments.
+From CFML Require Import WPLib Stdlib.
+From TLC Require Import LibOrder LibInt.
+Generalizable Variables A.
+
+Implicit Types n m : int.
+
+From EXAMPLES Require Import Basic_ml.
+
+
+(* ####################################################### *)
+(** * Proofs *)
+
+Lemma incr_spec : forall p n,
+  SPEC (incr p)
+    PRE (p ~~> n)
+    POSTUNIT (p ~~> (n+1)).
+Proof. xcf. xapp. xapp. xsimpl. Qed.
+
+#[global] Hint Extern 1 (RegisterSpec incr) => Provide incr_spec.
+
+Lemma example_let_spec : forall n,
+  SPEC (example_let n)
+    PRE \[]
+    POST (fun r => \[r = 2*n]).
+Proof using. xcf. xlet. xlet. xval. xsimpl. math. Qed.
+
+Lemma quadruple_spec : forall n,
+  SPEC (quadruple n)
+    PRE \[]
+    POST (fun r => \[r = 4 * n]).
+Proof. xcf. xlet. xval. xsimpl. math. Qed.
+
+Lemma inplace_double_spec : forall p n,
+  SPEC (inplace_double p)
+    PRE (p ~~> n)
+    POSTUNIT (p ~~> (2*n)).
+Proof. xcf. xapp. xlet. xapp. xsimpl. math. Qed.
+
+Lemma incr_two_spec : forall (p q:loc) (n m:int),
+  SPEC (incr_two p q)
+    PRE (p ~~> n \* q ~~> m)
+    POSTUNIT (p ~~> (n+1) \* q ~~> (m+1)).
+Proof using. xcf. xapp. xapp. xsimpl. Qed.
+
+#[global] Hint Extern 1 (RegisterSpec incr_two) => Provide incr_two_spec.
+
+Lemma incr_two_aliased_spec : forall p n,
+  SPEC (incr_two p p)
+    PRE (p ~~> n)
+    POSTUNIT (p ~~> (n+2)).
+Proof using. xcf. xapp. xapp. xsimpl. math. Qed.
+
+Lemma aliased_call_spec : forall p n,
+  SPEC (aliased_call p)
+    PRE (p ~~> n)
+    POSTUNIT (p ~~> (n+2)).
+Proof using. xcf. xapp incr_two_aliased_spec. xsimpl. Qed.
+
+Lemma incr_first_spec : forall p q n m,
+  SPEC (incr_first p q)
+    PRE (p ~~> n \* q ~~> m)
+    POSTUNIT (p ~~> (n+1) \* q ~~> m).
+Proof using. xcf. xapp. xsimpl. Qed.
+
+Lemma incr_first_spec' : forall p (q:loc) n,
+  SPEC (incr_first p q)
+    PRE (p ~~> n)
+    POSTUNIT (p ~~> (n+1)).
+Proof using. xcf. xapp. xsimpl. Qed.
+
+Lemma incr_first_derived_spec : forall p q n m,
+  SPEC (incr_first p q)
+    PRE (p ~~> n \* q ~~> m)
+    POSTUNIT (p ~~> (n+1) \* q ~~> m).
+Proof using. intros. xapp incr_first_spec'. xsimpl. Qed.
+
+Lemma transfer_spec : forall p q n m,
+  SPEC (transfer p q)
+    PRE (p ~~> n \* q ~~> m)
+    POSTUNIT (p ~~> (n + m) \* q ~~> 0).
+Proof using. xcf. xapp. xapp. xlet. xapp. xapp. xsimpl. math. Qed.
+
+Lemma transfer_aliased_spec : forall p n,
+  SPEC (transfer p p)
+    PRE (p ~~> n)
+    POSTUNIT (p ~~> 0).
+Proof using. xcf. xapp. xapp. xlet. xapp. xapp. xsimpl. Qed.
+
+Lemma ref_greater_spec : forall p n,
+  SPEC (ref_greater p)
+    PRE (p ~~> n)
+    POST (fun q => p ~~> n \* q ~~> (n+1)).
+Proof using. xcf. xapp. xlet. xapp. intros q. xsimpl. math. Qed.
+
+Lemma ref_greater_abstract_spec : forall p n,
+  SPEC (ref_greater p)
+    PRE (p ~~> n)
+    POST (fun q => \exists m, \[m > n] \* q ~~> m \* p ~~> n).
+Proof using. intros. xapp ref_greater_spec. xsimpl. { math. } Qed.
+
+Lemma succ_using_incr_spec : forall n,
+  SPEC (succ_using_incr n)
+    PRE \[]
+    POST (fun r => \[r = n+1]).
+Proof using. xcf. xapp. intros p. xapp. xapp. xsimpl. auto. Qed.
+
+Lemma get_and_free_spec : forall p `{Enc A} (v:A),
+  SPEC (get_and_free p)
+    PRE (p ~~> v)
+    POST (fun r => \[r = v]).
+Proof using. xcf. xapp. xval. xsimpl. auto. Qed.
+
+
+Module Import Facto.
+
+Parameter facto : int -> int.
+
+Parameter facto_init : forall n,
+  0 <= n <= 1 ->
+  facto n = 1.
+
+Parameter facto_step : forall n,
+  n > 1 ->
+  facto n = n * (facto (n-1)).
+
+End Facto.
+
+Lemma factorec_spec : forall n,
+  n >= 0 ->
+  SPEC (factorec n)
+    PRE \[]
+    POST (fun r => \[r = facto n]).
+Proof using.
+  intros n. induction_wf IH: (downto 0) n.
+  intros Hn. xcf. xif.
+  { intros C. xval. xsimpl.
+    rewrite facto_init. math. math. }
+  { intros C. xapp. { hnf. math. } { math. }
+    xval. xsimpl. rewrite (@facto_step n). math. math. }
+Qed.
+
+
+Lemma repeat_incr_spec : forall (m n:int) p,
+  m >= 0 ->
+  SPEC (repeat_incr p m)
+    PRE (p ~~> n)
+    POSTUNIT (p ~~> (n + m)).
+Proof using.
+  intros m. induction_wf IH: (downto 0) m. unfold downto in IH.
+  intros n p Hm. xcf. xif; intros C.
+  { xapp. xapp. { math. } { math. } xsimpl. math. }
+  { xval. xsimpl. math. }
+Qed.
+
+Lemma step_transfer_spec : forall p q n m,
+  m >= 0 ->
+  SPEC (step_transfer p q)
+    PRE (p ~~> n \* q ~~> m)
+    POSTUNIT (p ~~> (n + m) \* q ~~> 0).
+Proof using.
+  intros q p n m Hm.
+  revert n Hm. induction_wf IH: (downto 0) m. unfold downto in IH.
+  intros. xcf. xapp. xif; intros C.
+  { xapp. xapp. xapp. { math. } { math. } xsimpl. math. }
+  { xval. xsimpl. { math. } { math. } }
+Qed.
