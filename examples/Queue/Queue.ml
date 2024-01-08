3e7e30e112a95a6042370cd5cecc95a952bf9019
Mário Pereira <mjp.pereira@fct.unl.pt>
2024-01-08 22:56:21 +0000

    [Queue]: added and proved [push] function.
      - TODO: the proof is ugly at the moment; need to clean-up.

--- examples/Queue/Queue.ml
+++ examples/Queue/Queue.ml
@@ -11,3 +11,12 @@ let create () = {
 }
 
 let is_empty q = q.size = 0
+
+let push x q =
+  if is_empty q then begin
+    let l = MList.create () in
+    MList.push l x;
+    q.front <- l end
+  else
+    MList.push q.rear x;
+  q.size <- q.size + 1
--- examples/Queue/Queue_proof.v
+++ examples/Queue/Queue_proof.v
@@ -4,7 +4,7 @@ From CFML Require Import Stdlib.
 From EXAMPLES Require Import MList_ml Queue_ml.
 From EXAMPLES Require Import MList_proof.
 From TLC Require Import LibListZ.
-Import SpecMListOf.
+Import SpecMList.
 
 Definition MQueue  A `{EA:Enc A} (L: list A) (q: loc) : hprop :=
   \exists (f r: loc) L1 L2, (q ~~~> `{ front' := f; rear' := r; size' := length L }) \*
@@ -41,6 +41,40 @@ Proof using.
   xapp. xvals*. split.
   { apply length_zero_inv. }
   { intros Hnil. rewrite Hnil. auto. }
+Qed.
+
+Lemma Triple_push : forall L (x: A) (q: loc),
+    SPEC (push x q)
+      PRE (MQueue L q)
+      POST (fun (_:unit) => MQueue (L & x) q).
+Proof using.
+  xcf.
+  xapp* Triple_is_empty ;=>.
+  destruct L as [|x' L'].
+  { Search (isTrue _).
+    rewrite <- LibListExec.is_nil_eq in H.
+    simpl in *. subst.
+    xif; intros; tryfalse *.
+    { xapp. intros.
+      xapp. unfold MQueue. xpull* ;=>.
+      xapp. xapp. xapp. xsimpl*.
+      { intros false. inversion false. }
+      { rew_list*. f_equal.
+        apply nil_eq_app_inv in H1.
+        destruct H1. auto. } } }
+  { Search (_ :: _ = nil).
+    cuts * G : (~ x' :: L' = nil).
+    { Search isTrue _.
+      apply isTrue_eq_false in G.
+      subst. xif; intros; tryfalse *.
+      unfold MQueue. xpull ;=>.
+      xapp. xapp. xapp. xapp. xsimpl*.
+      { rew_list*. math. }
+      { intros. assert (x2 = nil) by auto.
+        apply H0 in H2. subst.
+        rew_list* in *. }
+      { rewrite H1. rew_list*. } }
+    { intros false. inversion false. } }
 Qed.
 
 End Ops.
